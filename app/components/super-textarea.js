3e0fac97f053ae634a592ecf119dce06ef460efd
J-C HERON <myaddressmail@gmail.com>
2021-03-11 08:38:59 +0100

    add compo SuperTextarea

--- app/components/super-textarea.js
+++ app/components/super-textarea.js
@@ -2,12 +2,19 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class SuperTextareaComponent extends Component {
+  max;
   @tracked text;
   constructor() {
     super(...arguments);
-    this.text=this.args.value;
+    this.text = this.args.value;
+    this.max=this.args.maxLength;
   }
   get size() {
-    return this.text.length;
+    if (this.text)
+      return this.text.length;
+    return 0;
+  }
+  get isFull() {
+    return this.max <= this.size;
   }
 }
